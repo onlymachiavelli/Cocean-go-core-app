de37ce10b971c61ae4577166088d12c3721a1b6c
Lord Machiavelli <tmakaveli643@gmail.com>
2024-05-02 16:52:06 +0100

    fix : removing comments

--- src/routes/clients.go
+++ src/routes/clients.go
@@ -12,17 +12,6 @@ func ClientRoute(g *echo.Group, db *gorm.DB) {
 
 	cGrp.Use(middlewares.AuthMiddleware)
 
-	// businessGroup.POST("", func(c echo.Context) error {
-	// 	return handlers.CreateBusiness(c, db)
-	// })
-	// businessGroup.GET("", func(c echo.Context) error {
-	// 	return handlers.GetMyBusinesses(c, db)
-	// })
-	// businessGroup.GET("/one/:id", func(c echo.Context) error {
-	// 	return handlers.GetMyBusiness(c, db)
-	// })
-
-	//create order 
 	cGrp.GET("/:id", func(c echo.Context) error {
 		return handlers.GetMyClients(c, db)
 	})
